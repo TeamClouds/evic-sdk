279ed5f03f3b9c2a7c2eebf57a161f063f232ddc
Stephen Bird <sbird@cyngn.com>
2016-04-30 22:07:02 -0700

    WIP: implement mode settings

--- example/new_fw/settings.c
+++ example/new_fw/settings.c
@@ -188,6 +188,10 @@ struct menuDefinition displaySettingsMenu = {
     .menuItems = &displaySubMenuItems,
 };
 
+void showModeSettings() {
+	g.vapeModes[s.mode]->settings();
+}
+
 struct menuItem settingsMenuItems[] = {
     {
         .type = SELECT,
@@ -211,6 +215,15 @@ struct menuItem settingsMenuItems[] = {
         .populateCallback = &populateModes,
         .selectCallback = &updateMode,
     },
+    {
+        .type = SPACE,
+        .rows = 2,
+    },
+	{
+		.type = ACTION,
+		.label = "Mode Settings",
+		.actionCallback = &showModeSettings,
+	},
     {
         .type = SPACE,
         .rows = 2,
